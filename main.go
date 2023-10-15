60346188423efd2031568722c1c47b5d89ff6e22
Bartlomiej Rutkowski <brutkowski@tilt.app>
2023-10-15 14:54:24 +0200

    fix: Change address

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-
 	"github.com/rutkowskib/terraform-provider-violet/internal/provider"
 )
 
@@ -39,12 +38,6 @@ func main() {
 	flag.Parse()
 
 	opts := providerserver.ServeOpts{
-		// NOTE: This is not a typical Terraform Registry provider address,
-		// such as registry.terraform.io/hashicorp/hashicups. This specific
-		// provider address is used in these tutorials in conjunction with a
-		// specific Terraform CLI configuration for manual development testing
-		// of this provider.
-		Address: "hashicorp.com/edu/violet",
 		Debug:   debug,
 	}
 
